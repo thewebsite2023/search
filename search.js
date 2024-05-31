42cdc1f038450196caf952610cae5104b428a1eb
thewebsite2023 <nhattiento2704@gmail.com>
2024-05-31 22:25:00 +0700

    Update search.js

--- search.js
+++ search.js
@@ -1,20 +1,13 @@
-/* global instantsearch algoliasearch */
-
 app({
-    appId: 'latency',
-    apiKey: '6be0576ff61c053d5f9a3225e2a90f76',
-    indexName: 'instant_search',
+    appId: 'VD97WMT6IW',
+    apiKey: 'b8bbe11398038f430bc421bd75e9042a',
+    indexName: 'dev_index',
     searchParameters: {
       hitsPerPage: 10,
     },
   });
   
   function app(opts) {
-    // ---------------------
-    //
-    //  Init
-    //
-    // ---------------------
     const search = instantsearch({
       searchClient: algoliasearch(opts.appId, opts.apiKey),
       indexName: opts.indexName,
@@ -22,15 +15,10 @@ app({
       searchFunction: opts.searchFunction,
     });
   
-    // ---------------------
-    //
-    //  Default widgets
-    //
-    // ---------------------
     search.addWidgets([
       instantsearch.widgets.searchBox({
         container: '#search-input',
-        placeholder: 'Type here to search',
+        placeholder: 'Type here to search the documents',
       }),
       instantsearch.widgets.hits({
         container: '#hits',
@@ -77,119 +65,16 @@ app({
       //  Filtering widgets
       //
       // ---------------------
-      instantsearch.widgets.panel({
-        templates: {
-          header: getHeaderTemplate('category'),
-        },
-      })(instantsearch.widgets.hierarchicalMenu)({
-        container: '#hierarchical-categories',
-        attributes: [
-          'hierarchicalCategories.lvl0',
-          'hierarchicalCategories.lvl1',
-          'hierarchicalCategories.lvl2',
-        ],
-        showParentLevel: true,
-        templates: {
-          item:
-            '<a href="{{url}}" class="facet-item {{#isRefined}}active{{/isRefined}}"><span class="facet-name"><i class="fa fa-angle-right"></i> {{label}}</span class="facet-name"><span class="ais-HierarchicalMenu-count">{{count}}</span></a>', // eslint-disable-line
-        },
-      }),
-      instantsearch.widgets.panel({
-        templates: {
-          header: getHeaderTemplate('brand'),
-        },
-      })(instantsearch.widgets.refinementList)({
-        container: '#brand',
-        attribute: 'brand',
-        limit: 5,
-        showMore: true,
-        showMoreLimit: 10,
-        searchable: true,
-        searchablePlaceholder: 'Search for brands',
-        templates: {
-          searchableNoResults:
-            '<div class="sffv_no-results">No matching brands.</div>',
-          showMoreText: `
-            {{#isShowingMore}}
-              <span class="isShowingLess"></span>
-              Show less
-            {{/isShowingMore}}
-            {{^isShowingMore}}
-              <span class="isShowingMore"></span>
-              Show more
-            {{/isShowingMore}}
-          `,
-        },
-      }),
-      instantsearch.widgets.panel({
-        templates: {
-          header: getHeaderTemplate('price'),
-        },
-      })(instantsearch.widgets.rangeSlider)({
-        container: '#price',
-        attribute: 'price',
-        tooltips: {
-          format(rawValue) {
-            return `$${Math.round(rawValue).toLocaleString()}`;
-          },
-        },
-      }),
-      instantsearch.widgets.panel({
-        templates: {
-          header: getHeaderTemplate('rating'),
-        },
-      })(instantsearch.widgets.ratingMenu)({
-        container: '#stars',
-        attribute: 'rating',
-        max: 5,
-        labels: {
-          andUp: '& Up',
-        },
-      }),
-      instantsearch.widgets.panel({
-        templates: {
-          header: getHeaderTemplate('shipping'),
-        },
-      })(instantsearch.widgets.toggleRefinement)({
-        container: '#free-shipping',
-        attribute: 'free_shipping',
-        label: 'Free Shipping',
-        values: {
-          on: true,
-        },
-      }),
-      instantsearch.widgets.panel({
-        templates: {
-          header: getHeaderTemplate('type'),
-        },
-      })(instantsearch.widgets.menu)({
-        container: '#type',
-        attribute: 'type',
-        limit: 10,
-        showMore: true,
-        templates: {
-          showMoreText: `
-            {{#isShowingMore}}
-              <span class="isShowingLess"></span>
-              Show less
-            {{/isShowingMore}}
-            {{^isShowingMore}}
-              <span class="isShowingMore"></span>
-              Show more
-            {{/isShowingMore}}
-          `,
-        },
-      }),
+
+      
+      
+     
+      
     ]);
   
     search.start();
   }
   
-  // ---------------------
-  //
-  //  Helper functions
-  //
-  // ---------------------
   function getTemplate(templateName) {
     return document.querySelector(`#${templateName}-template`).innerHTML;
   }
